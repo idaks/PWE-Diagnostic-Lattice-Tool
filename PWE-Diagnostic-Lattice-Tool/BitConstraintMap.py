44067c19b943f2d7d68aee169c35adaf3e2089bd
sjgupta2 <sjgupta2@illinois.edu>
2020-02-13 20:07:15 -0600

    added lists of sat, unsat, amb nodes in BitCmap, progress in LatticeViz

--- PWE-Diagnostic-Lattice-Tool/BitConstraintMap.py
+++ PWE-Diagnostic-Lattice-Tool/BitConstraintMap.py
@@ -7,6 +7,7 @@ from .LatticeNode import (
     NodeAmbiguityType,
 )
 from collections import defaultdict
+import itertools
 from iteration_utilities import first
 from .PowersetBitLib import PowersetBitLib
 
@@ -19,6 +20,9 @@ class BitConstraintMap(ConstraintMap):
         self.constraint_to_int_map = dict(zip(constraints, range(self.num_constraints)))
         self.unexplored_set = set(range(2 ** self.num_constraints))
         self.explored_set = set([])
+        self.satisfiable_set = set([])
+        self.unsatisfiable_set = set([])
+        self.ambiguous_set = set([])
 
     def int_to_constraint_set(self, n):
         return [self.constraints[self.num_constraints - i - 1] for i in range(self.num_constraints - 1, -1, -1) if
@@ -50,23 +54,50 @@ class BitConstraintMap(ConstraintMap):
         self.nodes[self.__constraints_to_int_helper__(constraints)].update_num_pws(num_pws=num_pws,
                                                                                    num_pws_eval_type=num_pws_eval_type)
 
-    def check_node_num_pws(self, constraints):
+    def _check_node_num_pws_explicit_(self, constraints, check_against_memoized_sets=True):
 
         n = self.__constraints_to_int_helper__(constraints)
-
-        # Explicit Check
         if n in self.nodes:
             num_pws, num_pws_type = self.nodes[n].get_num_pws()
             if num_pws_type != NumPWSType.unevaluated:
                 return num_pws, num_pws_type
 
-        # Implicit Check
-        is_sat = self._check_node_sat_implicit_(constraints)
+        if check_against_memoized_sets:
+            if n in self.ambiguous_set:
+                return 2, NumPWSType.atleast
+            if n in self.satisfiable_set:
+                return 1, NumPWSType.atleast
+            if n in self.unsatisfiable_set:
+                return 0, NumPWSType.exact
+            if n in self.minimal_unambiguous_constraint_subsets:
+                return 1, NumPWSType.exact
+
+        return None, None
+
+    def _check_node_num_pws_implicit_(self, constraints):
+
+        n = self.__constraints_to_int_helper__(constraints)
+        is_sat = self._check_node_sat_implicit_(n)
         if is_sat is not None:
             if is_sat:
                 return 1, NumPWSType.atleast
             else:
                 return 0, NumPWSType.exact
+        return None, None
+
+    def check_node_num_pws(self, constraints):
+
+        n = self.__constraints_to_int_helper__(constraints)
+
+        # Explicit Check
+        explicit_check_result = self._check_node_num_pws_explicit_(n)
+        if explicit_check_result[0] is not None:
+            return explicit_check_result
+
+        # Implicit Check
+        implicit_check_result = self._check_node_num_pws_implicit_(n)
+        if implicit_check_result[0] is not None:
+            return implicit_check_result
 
         # No idea based on current information
         return -1, NumPWSType.unevaluated
@@ -85,8 +116,15 @@ class BitConstraintMap(ConstraintMap):
 
         return NodeEvalState.unevaluated
 
-    def _check_node_sat_explicit_(self, constraints):
+    def _check_node_sat_explicit_(self, constraints, check_against_memoized_sets=True):
         n = self.__constraints_to_int_helper__(constraints)
+
+        if check_against_memoized_sets:
+            if n in self.satisfiable_set:
+                return True
+            if n in self.unsatisfiable_set:
+                return False
+
         if n in self.nodes:
             return self.nodes[n].is_sat()
         return None
@@ -128,6 +166,17 @@ class BitConstraintMap(ConstraintMap):
         else:
             muas_es = set(map(self.__constraints_to_int_helper__, muas_es))
 
+        # Necessary short circuiting since the for loops below only work assuming n is not in these sets
+        # O/w for example if n == c and c is an MUS, n is an ancestor of c, but shouldn't be satisfiable
+        if n in mus_es:
+            return False
+        if n in mss_es:
+            return True
+        if n in muas_es:
+            return True
+        if n in mas_es:
+            return True
+
         for c in mus_es.union(mss_es):
             # Can optimize by doing the conversion of c to int here, so it's only converted if needed
             if PowersetBitLib.is_ancestor(n, c):
@@ -144,14 +193,22 @@ class BitConstraintMap(ConstraintMap):
     def check_sat(self, constraints):
         constraints = self.__constraints_to_int_helper__(constraints)
         k = self._check_node_sat_explicit_(constraints)
-        if k is not None:
-            return k
+        # if k is not None:
+        #     return k
+        #
+        # return self._check_node_sat_implicit_(constraints)
+        return k
 
-        return self._check_node_sat_implicit_(constraints)
-
-    def _check_node_ambiguity_explicit_(self, constraints):
+    def _check_node_ambiguity_explicit_(self, constraints, check_against_memoized_sets=True):
         # explicit check in the self.nodes dict
         n = self.__constraints_to_int_helper__(constraints)
+
+        if check_against_memoized_sets:
+            if n in self.ambiguous_set:
+                return NodeAmbiguityType.ambiguous
+            elif n in self.unsatisfiable_set:
+                return NodeAmbiguityType.unsat
+
         if n in self.nodes:
             return self.nodes[n].is_ambiguous()
         return None
@@ -192,6 +249,15 @@ class BitConstraintMap(ConstraintMap):
         else:
             muas_es = set(map(self.__constraints_to_int_helper__, muas_es))
 
+        if n in mas_es:
+            return NodeAmbiguityType.ambiguous
+        elif n in muas_es:
+            return NodeAmbiguityType.unambiguous
+        elif n in mus_es:
+            return NodeAmbiguityType.unsat
+        elif n in mss_es:
+            return None
+
         for c in mas_es.union(muas_es):
             # Can optimize by doing the conversion of c to int here, so it's only converted if needed
             if PowersetBitLib.is_ancestor(n, c):
@@ -210,9 +276,10 @@ class BitConstraintMap(ConstraintMap):
         # Explicit Check
         constraints = self.__constraints_to_int_helper__(constraints)
         k = self._check_node_ambiguity_explicit_(constraints)
-        if k is not None:
-            return k
-        return self._check_node_ambiguity_implicit_(constraints)
+        # if k is not None:
+        #     return k
+        # return self._check_node_ambiguity_implicit_(constraints)
+        return k
 
     def get_unexplored(self, return_seed_int=False):
 
@@ -277,14 +344,25 @@ class BitConstraintMap(ConstraintMap):
                     if sat_check:
                         seed.add(c)
                         seed_int = seed_plus_c_int
-                    if update_map_with_intermediate_results:
-                        if sat_check is True:
-                            self.update_num_pws(seed_plus_c_int, num_pws=1, num_pws_eval_type=NumPWSType.atleast)
-                        else:  # if sat_check is False:
+                    else:
+                        if update_map_with_intermediate_results:
                             self.update_num_pws(seed_plus_c_int, num_pws=0, num_pws_eval_type=NumPWSType.exact)
+
+                    # if update_map_with_intermediate_results:
+                    #     if sat_check is True:
+                    #         # """Do we really need this case? If it is true,
+                    #         #    it will become a ancestor of the eventual MSS"""
+                    #         self.update_num_pws(seed_plus_c_int, num_pws=1, num_pws_eval_type=NumPWSType.atleast)
+                    #     else:  # if sat_check is False:
+                    #         self.update_num_pws(seed_plus_c_int, num_pws=0, num_pws_eval_type=NumPWSType.exact)
 
         if update_map_with_mss:
             self.maximal_satisfiable_constraint_subsets.add(seed_int)
+            sat_ancestors = set(PowersetBitLib.get_ancestors(seed_int, self.num_constraints))
+            unsat_descendants = set(PowersetBitLib.get_descendants(seed_int, self.num_constraints)).difference(
+                {seed_int})
+            self.satisfiable_set.update(sat_ancestors)
+            self.unsatisfiable_set.update(unsat_descendants)
 
         if return_mss_int:
             return seed, seed_int
@@ -316,14 +394,22 @@ class BitConstraintMap(ConstraintMap):
                     if not sat_check:
                         seed.remove(c)
                         seed_int = seed_minus_c_int
-                    if update_map_with_intermediate_results:
-                        if sat_check is True:
+                    else:
+                        if update_map_with_intermediate_results:
                             self.update_num_pws(seed_minus_c_int, num_pws=1, num_pws_eval_type=NumPWSType.atleast)
-                        else:  # if sat_check is False:
-                            self.update_num_pws(seed_minus_c_int, num_pws=0, num_pws_eval_type=NumPWSType.exact)
+
+                    # if update_map_with_intermediate_results:
+                    #     if sat_check is True:
+                    #         self.update_num_pws(seed_minus_c_int, num_pws=1, num_pws_eval_type=NumPWSType.atleast)
+                    #     else:  # if sat_check is False:
+                    #         self.update_num_pws(seed_minus_c_int, num_pws=0, num_pws_eval_type=NumPWSType.exact)
 
         if update_map_with_mus:
             self.minimal_unsatisfiable_constraint_subsets.add(seed_int)
+            sat_ancestors = set(PowersetBitLib.get_ancestors(seed_int, self.num_constraints)).difference({seed_int})
+            unsat_descendants = set(PowersetBitLib.get_descendants(seed_int, self.num_constraints))
+            self.satisfiable_set.update(sat_ancestors)
+            self.unsatisfiable_set.update(unsat_descendants)
 
         if return_mus_int:
             return seed, seed_int
@@ -353,16 +439,26 @@ class BitConstraintMap(ConstraintMap):
                     if amb_check == NodeAmbiguityType.ambiguous:
                         seed.add(c)
                         seed_int = seed_plus_c_int
-                    if update_map_with_intermediate_results:
-                        if amb_check == NodeAmbiguityType.ambiguous:
-                            self.update_num_pws(seed_plus_c_int, num_pws=2, num_pws_eval_type=NumPWSType.atleast)
-                        elif amb_check == NodeAmbiguityType.unambiguous:
-                            self.update_num_pws(seed_plus_c_int, num_pws=1, num_pws_eval_type=NumPWSType.exact)
-                        elif amb_check == NodeAmbiguityType.unsat:
-                            self.update_num_pws(seed_plus_c_int, num_pws=0, num_pws_eval_type=NumPWSType.exact)
+                    else:
+                        if update_map_with_intermediate_results:
+                            if amb_check == NodeAmbiguityType.unambiguous:
+                                self.update_num_pws(seed_plus_c_int, num_pws=1, num_pws_eval_type=NumPWSType.exact)
+                            elif amb_check == NodeAmbiguityType.unsat:
+                                self.update_num_pws(seed_plus_c_int, num_pws=0, num_pws_eval_type=NumPWSType.exact)
+
+                    # if update_map_with_intermediate_results:
+                    #     if amb_check == NodeAmbiguityType.ambiguous:
+                    #         self.update_num_pws(seed_plus_c_int, num_pws=2, num_pws_eval_type=NumPWSType.atleast)
+                    #     elif amb_check == NodeAmbiguityType.unambiguous:
+                    #         self.update_num_pws(seed_plus_c_int, num_pws=1, num_pws_eval_type=NumPWSType.exact)
+                    #     elif amb_check == NodeAmbiguityType.unsat:
+                    #         self.update_num_pws(seed_plus_c_int, num_pws=0, num_pws_eval_type=NumPWSType.exact)
 
         if update_map_with_mas:
             self.maximal_ambiguous_constraint_subsets.add(seed_int)
+            amb_ancestors = set(PowersetBitLib.get_ancestors(seed_int, self.num_constraints))
+            self.ambiguous_set.update(amb_ancestors)
+            self.satisfiable_set.update(amb_ancestors)
 
         if return_mas_int:
             return seed, seed_int
@@ -393,6 +489,9 @@ class BitConstraintMap(ConstraintMap):
                     if amb_check == NodeAmbiguityType.unambiguous:
                         seed.remove(c)
                         seed_int = seed_minus_c_int
+
+                    # Keep in this case because the blocked sections are a mix of UNAMB (==1PW) and UNSAT,
+                    # so this information is still useful
                     if update_map_with_intermediate_results:
                         if amb_check == NodeAmbiguityType.ambiguous:
                             self.update_num_pws(seed_minus_c_int, num_pws=2, num_pws_eval_type=NumPWSType.atleast)
@@ -403,7 +502,71 @@ class BitConstraintMap(ConstraintMap):
 
         if update_map_with_muas:
             self.minimal_unambiguous_constraint_subsets.add(seed_int)
+            amb_ancestors = set(PowersetBitLib.get_ancestors(seed_int, self.num_constraints)).difference({seed_int})
+            self.ambiguous_set.update(amb_ancestors)
+            self.satisfiable_set.update(amb_ancestors)
+            self.satisfiable_set.add(seed_int)
 
         if return_muas_int:
             return seed, seed_int
         return seed
+
+    def get_all_nodes_num_pws(self):
+
+        num_pws = {k: (None, None) for k in range(2**self.num_constraints)}
+
+        for n in self.satisfiable_set.difference(self.ambiguous_set):
+            explicit_check = self._check_node_num_pws_explicit_(n, check_against_memoized_sets=False)
+            num_pws[n] = explicit_check if explicit_check[0] is not None else (1, NumPWSType.atleast)
+        for n in self.unsatisfiable_set:
+            num_pws[n] = (0, NumPWSType.exact)
+        for n in self.ambiguous_set:
+            explicit_check = self._check_node_num_pws_explicit_(n, check_against_memoized_sets=False)
+            if explicit_check[0] is not None:
+                if explicit_check[1] == NumPWSType.atleast:
+                    num_pws[n] = (max(2, explicit_check[0]), NumPWSType.atleast)
+                elif explicit_check[1] == NumPWSType.exact:
+                    num_pws[n] = explicit_check
+                elif explicit_check[1] == NumPWSType.unevaluated:
+                    num_pws[n] = (2, NumPWSType.atleast)
+            else:
+                num_pws[n] = (2, NumPWSType.atleast)
+        for n in self.minimal_unambiguous_constraint_subsets:
+            num_pws[n] = (1, NumPWSType.exact)
+
+        pot_missed_nodes = set(itertools.chain.from_iterable(
+            PowersetBitLib.get_descendants(n, 4) for n in self.minimal_unambiguous_constraint_subsets)
+        ).difference(self.minimal_unambiguous_constraint_subsets)\
+            .difference(self.satisfiable_set)\
+            .difference(self.unsatisfiable_set)
+
+        unexplored = self.unexplored_set.union(pot_missed_nodes)
+        for n in unexplored:
+            num_pws[n] = self._check_node_num_pws_explicit_(n, check_against_memoized_sets=False)
+
+        return num_pws
+
+    def get_all_nodes_sat(self):
+
+        node_to_sat = {k: None for k in range(2**self.num_constraints)}
+
+        for n in self.satisfiable_set:
+            node_to_sat[n] = True
+        for n in self.unsatisfiable_set:
+            node_to_sat[n] = False
+        for n in node_to_sat.keys():
+            if node_to_sat[n] is None:
+                node_to_sat[n] = self._check_node_sat_explicit_(n, check_against_memoized_sets=False)
+
+        return node_to_sat
+
+    # TODO
+    def get_all_nodes_ambiguity_status(self):
+        pass
+
+
+
+
+
+
+
--- PWE-Diagnostic-Lattice-Tool/PowersetLatticeViz.py
+++
@@ -1,7 +0,0 @@
-from .ConstraintMap import ConstraintMap
-
-
-class PowersetLatticeViz:
-
-    def __init__(self, cmap: ConstraintMap):
-        self.cmap = cmap
